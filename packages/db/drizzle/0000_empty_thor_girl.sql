7d9f8ae9f9d3348b6d1b927c7098bd637f2ed6a7
Daniils <iidanl@icloud.com>
2026-07-23 03:31:25 +0800

    fix: updated migrations

--- packages/db/drizzle/0000_empty_thor_girl.sql
+++ packages/db/drizzle/0000_empty_thor_girl.sql
@@ -123,7 +123,6 @@ CREATE TABLE "tags" (
 --> statement-breakpoint
 CREATE TABLE "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"github_id" text NOT NULL,
 	"username" text NOT NULL,
 	"name" text,
 	"email" text,
@@ -131,7 +130,7 @@ CREATE TABLE "users" (
 	"role" text DEFAULT 'user' NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
 	"updated_at" timestamp with time zone DEFAULT now() NOT NULL,
-	CONSTRAINT "users_github_id_unique" UNIQUE("github_id")
+	CONSTRAINT "users_username_unique" UNIQUE("username")
 );
 --> statement-breakpoint
 ALTER TABLE "audit_logs" ADD CONSTRAINT "audit_logs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
